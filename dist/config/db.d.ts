fe4c0820ded99e2cbc2ca8beb88ce2d9ba804945
DevAlexandreCR <devalexandrecr@gmail.com>
2025-12-09 19:27:46 -0500

    refactor: remove unused files and clean up dist directory

--- dist/config/db.d.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-declare const prisma: PrismaClient<{
-    datasourceUrl: string;
-    log: ("error" | "warn")[];
-}, never, import("@prisma/client/runtime/library").DefaultArgs>;
-export declare const connectToDatabase: () => Promise<void>;
-export declare const disconnectFromDatabase: () => Promise<void>;
-export { prisma };
-//# sourceMappingURL=db.d.ts.map
